fd3c1001c9ac43590f75b171eaea9a9c42e43518
sdhol <shreyash.dholakia@monsanto.com>
2016-04-05 16:17:12 -0500

    class 101 spring boot

---
+++ src/main/java/com/springBootDemo/Widget.java
@@ -0,0 +1,31 @@
+package com.springBootDemo;
+
+/**
+ * Created by sdhol on 4/5/16.
+ */
+public class Widget {
+
+    private String type;
+
+    private int length;
+
+    private int height;
+
+    public Widget(String type, int length, int height) {
+        this.type = type;
+        this.length = length;
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ src/main/java/com/springBootDemo/WidgetController.java
@@ -0,0 +1,20 @@
+package com.springBootDemo;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sdhol on 4/5/16.
+ */
+@RequestMapping("/api")
+@RestController
+public class WidgetController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Widget index() {
+        return new Widget("green", 10, 7);
+    }
+}
